a5b6b2ca26fab11610cff64ffcc0ba3669e667bf
bocman <bocman@siol.net>
2015-09-03 20:00:23 +0200

    Changes Heroku

--- project/wsgi.py
+++ project/wsgi.py
@@ -19,6 +19,6 @@ djcelery.setup_loader()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-#application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+
