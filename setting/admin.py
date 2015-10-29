fb4e2707a6575a7c1c3870d331dce207be26565b
bocman <bocman@siol.net>
2015-10-29 13:52:28 +0100

    Registered models for Admin dashboard

--- setting/admin.py
+++ setting/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Client, Event, EventActivationElements
+from models import Client, Event, EventActivationElements
 
 admin.site.register(Client)
 admin.site.register(Event)
