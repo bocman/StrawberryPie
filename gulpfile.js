4fd547a7d3b62684abb20d289bfd2cf927568669
root <root@bostjan-HP-Pavilion-dv6-Notebook-PC.lan>
2016-01-29 00:40:39 +0100

    Added watch and CompileCSS gulp tasks + redesign of including css files

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,25 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	path = require('path'),
 	csso = require('gulp-csso'),
-	rev = require('gulp-rev'),
 	uglify = require('gulp-uglify'),
 	watch = require('gulp-watch');
+	md5 = require('gulp-md5'),
+	gulprimraf = require('gulp-rimraf');
 
 gulp.task('test', function() {
 	console.log("nisem ne ");
 });
 
+gulp.task('cleanPublicCSS', function () {
+   return gulp.src('./static/public/*.css', { read: false })
+     .pipe(gulprimraf());
+});
 
-gulp.task('compileCSS', function () {
-  var quoteSrc = './static/public/*.css'
-
-  return gulp.src('./static/*.less')
-    .pipe(less({
-        paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(rev())
+gulp.task('compileCSS', ['cleanPublicCSS'], function () {
+  return gulp.src('./static/bundle.less')
+    .pipe(less())
+    //.pipe(md5())
     .pipe(csso())
-    //.pipe(rimraf("./static/public/*.css"))
     .pipe(gulp.dest('./static/public/'));
 });
 
@@ -32,3 +32,17 @@ gulp.task('compileJS', function() {
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
+
+
+gulp.task('watch', function () {
+	console.log("Watch is active")
+	gulp.watch(['./static/**/*.js'], ['compileJS']);
+    gulp.watch(['./static/**/*.less'], ['compileCSS']);
+});
+
+
+
+
+
+
+
